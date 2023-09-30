454d1648713682e18a3b799853979475ef24552f
Abdul Qadir <aq98123@gmail.com>
2023-09-30 09:16:31 +0500

    Flask app done

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hellow python with Flask"
+    name = input("Enter your name:")
+
+    return render_template('home.html',data={'name':name})
+
+@app.route("/about",methods=["GET","POST"])
+def about():
+    if request.method == "POST":
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        result = num1 + num2
+
+        return render_template('about.html', results = {"result":result})
+
+    else:
+        return render_template("about.html", results = {'esult':''})
+
 
 if __name__ == "__main__":
     
